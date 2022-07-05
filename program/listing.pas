0ff3cc36a67c5c9690b9edcf222843e16c8b77c5
Ftthreign <98510825+Ftthreign@users.noreply.github.com>
2022-07-06 03:09:55 +0700

    Create listing.pas

---
+++ program/listing.pas
@@ -0,0 +1,19 @@
+Program Regresi;
+Uses crt;
+Var y, a, b: Integer;
+Begin 
+Clrscr;
+
+Writeln('==================');
+Write(' Input Nilai a = ');
+Readln(a);
+Write(' Input Nilai b = ');
+Readln(b);
+Writeln('==================');
+Writeln;
+y:=(6*a)+(2*b);
+Writeln(' Nilai Persamaan y = 6a + 2b = ',y);
+Writeln;
+Writeln(' Tekan Sembarang Tombol Untuk Mengakhiri ');
+Readkey;
+End.
