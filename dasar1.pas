d1f4ed2a287e454c990304931f52a9db8f538808
Ftthreign <98510825+Ftthreign@users.noreply.github.com>
2022-07-06 02:51:33 +0700

    Create dasar1.pas

---
+++ dasar1.pas
@@ -0,0 +1,18 @@
+Program hallo; 
+Uses crt;
+Clrscr;
+Writeln('====================');
+Writeln('Selamat Datang');
+Writeln('Perkenalkan');
+Writeln('Nama Saya');
+Write('Fadhil Abdul Fattah');
+Writeln('====================');
+Writeln;
+
+Write('Disini saya akan');
+Writeln('Menuliskan Sebuah kode pascal');
+Writeln('Menggunakan turbo pascal');
+Writeln;
+Write('Klik untuk tutup');
+Readln;
+End. 
